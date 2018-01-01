91f902f4b35c99a33c1ee3123c6daa1d6bec3e36
Sergey Batanov <sergey.batanov@dmpas.ru>
2018-01-01 23:20:55 +0300

    Choco 19

--- install/choco/tools/chocolateyinstall.ps1
+++ install/choco/tools/chocolateyinstall.ps1
@@ -17,8 +17,8 @@ $packageArgs = @{
   validExitCodes= @(0, 3010, 1641)
 
   registryUninstallerKey = 'onescript'
-  # 1.0.18 exe
-  checksum      = 'dbc49c92c933df69b90c41792e37d9221620821fe983eb149d71ab92593bdbb8'
+  # 1.0.19 exe
+  checksum      = '5250cd1a514c5f63014573f5ee33c041945db11ae15eb3024cad283cc1c51800'
   checksumType  = 'sha256'
   checksum64    = ''
   checksumType64= 'sha256'
